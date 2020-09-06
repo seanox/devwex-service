a0f51eb7b0c751d441b4e30a67d7198a4ac62428
seanox <service@seanox.de>
2020-09-06 08:10:28 +0200

    #0000 Sources: (Re-)Formatting, translation, code minimization

--- sources/com/seanox/devwex/Bootstrap.java
+++ sources/com/seanox/devwex/Bootstrap.java
@@ -1,48 +1,47 @@
 /**
- *  LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
- *  im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
- *  Diese Software unterliegt der Version 2 der Apache License.
+ * LIZENZBEDINGUNGEN - Seanox Software Solutions ist ein Open-Source-Projekt,
+ * im Folgenden Seanox Software Solutions oder kurz Seanox genannt.
+ * Diese Software unterliegt der Version 2 der GNU General Public License.
  *
- *  Devwex, Advanced Server Development
- *  Copyright (C) 2017 Seanox Software Solutions
- *  
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not
- *  use this file except in compliance with the License. You may obtain a copy
- *  of the License at
- *  
- *  http://www.apache.org/licenses/LICENSE-2.0
- *  
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *  License for the specific language governing permissions and limitations
- *  under the License.
+ * Devwex, Advanced Server Development
+ * Copyright (C) 2020 Seanox Software Solutions
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 2 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package com.seanox.devwex;
 
 /**
- *  Bootstrap steuert den Server bei den Kommandos {@code start},
- *  {@code restart} und {@code stop} alternativ an. Was f&uuml;r einen sauberen
- *  Aufruf als Service mit der {@code prunsrv.exe} erforderlich ist.<br>
- *  <br>
- *  Bootstrap 5.0.0 20171203<br>
- *  Copyright (C) 2017 Seanox Software Solutions<br>
- *  Alle Rechte vorbehalten.
+ * Bootstrap controls the server alternatively with the commands {@code start},
+ * {@code restart} and {@code stop}, which is necessary for a clean call as
+ * service with the {@code prunsrv.exe}.<br>
+ * <br>
+ * Bootstrap 5.0.1 20200905<br>
+ * Copyright (C) 2020 Seanox Software Solutions<br>
+ * Alle Rechte vorbehalten.
  *
- *  @author  Seanox Software Solutions
- *  @version 5.0.0 20171203
+ * @author  Seanox Software Solutions
+ * @version 5.0.1 20200905
  */
 public class Bootstrap {
 
     /**
-     *  Haupteinsprung in die Anwendung.
-     *  @param options Startargumente
+     * Main entry into the application.
+     * @param options Program arguments
      */
-    public static void main(String[] options) {
+    public static void main(String... options) {
 
-        String string;
-
-        string = "";
+        String string = "";
         if (options != null
                 && options.length > 0)
             string = options[0];
@@ -54,7 +53,6 @@ public class Bootstrap {
             Service.restart();
         } else if (string.equals("stop")) {
             Service.destroy();
-            System.exit(0);
         }
     }
 }
